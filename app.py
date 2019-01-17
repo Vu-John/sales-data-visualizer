bbfc6ec473a17b4d144352fd0faf222549bca0cb
Vu-John <johnvu1995@gmail.com>
2019-01-17 02:35:08 -0500

    User env port in app.js

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template
 from utils import sale_orders_data
 
@@ -18,4 +19,5 @@ def orders():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
